3bd072ad5db6cf07c1307b4035f7545e6ddbac4f
Gabriel Souza <rorato91@gmail.com>
2022-06-27 14:47:09 -0300

    update

--- view.py
+++ view.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+# Cores
 cor0 = "#f0f3f5"  # Preto
 cor1 = "#feffff"  # Branco
 cor2 = "#4fa882"  # Verde
@@ -11,10 +12,25 @@ cor7 = "#ef5350"  # Vermelho
 cor8 = "#263238"  # + verde
 cor9 = "#e9edf5"  # Sky blue
 
+# Configuração inicial da tela
 janela = Tk()
 janela.title('Consultas')
 janela.geometry('1043x453')
 janela.configure(background=cor9)
-janela.
+janela.resizable(width=FALSE, height=FALSE)
+
+# Divisão das Janelas
+frame_cima = Frame(janela, width=310, height=50, bg=cor2, relief='flat')
+frame_cima.grid(row=0, column=0)
+
+frame_baixo = Frame(janela, width=310, height=400, bg=cor1, relief='flat')
+frame_baixo.grid(row=1, column=0, padx=0, pady=1,sticky=NSEW)
+
+frame_direita = Frame(janela, width=588, height=403, bg=cor1, relief='flat')
+frame_direita.grid(row=0, column=1, rowspan=2, padx=1, pady=0,sticky=NSEW)
+
+# Tela cima
+app_nome = Frame(frame_cima, text='Formulário de Consulta', anchor=NW)
+app_nome.grid(row=0, column=0)
 
 janela.mainloop()
