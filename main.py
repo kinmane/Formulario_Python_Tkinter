aaf7823723cd84f1c287949d3002274ea25ad0a6
Gabriel Souza <rorato91@gmail.com>
2022-06-28 10:25:07 -0300

    Atualizar e Inserir
    criado a parte para inserir e atualizar no banco as tabelas

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from cgitb import text
 from tkinter import *
-from tkinter import font, ttk
+from tkinter import font, ttk, messagebox
 from tkcalendar import Calendar, DateEntry
+from view import *
 
 # Cores
 cor0 = "#f0f3f5"  # Preto
@@ -36,6 +37,87 @@ frame_direita.grid(row=0, column=1, rowspan=2, padx=1, pady=0,sticky=NSEW)
 app_nome = Label(frame_cima, text='Formulário de Consulta', anchor=NW, font=('Ivy 13 bold'), bg=cor2, fg=cor1, relief='flat')
 app_nome.place(x=10, y=20)
 
+global tree
+
+def inserir():
+    nome = entry_nome.get()
+    email = entry_email.get()
+    telefone = entry_telefone.get()
+    calendario = entry_calendario.get()
+    prioridade = entry_prioridade.get()
+    especialista = entry_especialista.get()
+
+    lista = [nome, email, telefone, calendario, prioridade, especialista]
+
+    if nome == '':
+        messagebox.showerror('Erro', 'O nome não pode ser vazio')
+    else:
+        inserir_info(lista)
+        messagebox.showinfo('Sucesso', 'Os dados foram inseridos com sucesso')
+
+        entry_nome.delete(0, 'end')
+        entry_email.delete(0, 'end')
+        entry_telefone.delete(0, 'end')
+        entry_calendario.delete(0, 'end')
+        entry_prioridade.delete(0, 'end')
+        entry_especialista.delete(0, 'end')
+
+    for widget in frame_direita.winfo_children():
+        widget.destroy()
+
+    mostrar()
+
+def atualizar():
+    try:
+        treeview_dados = tree.focus()
+        treeview_dicionario = tree.item(tree)
+        tree_lista = treeview_dicionario['values']
+
+        valor = tree_lista[0]
+
+        entry_nome.delete(0, 'end')
+        entry_email.delete(0, 'end')
+        entry_telefone.delete(0, 'end')
+        entry_calendario.delete(0, 'end')
+        entry_prioridade.delete(0, 'end')
+        entry_especialista.delete(0, 'end')
+
+        entry_nome.insert(0, tree_lista[1])
+        entry_email.insert(0, tree_lista[2])
+        entry_telefone.insert(0, tree_lista[3])
+        entry_calendario.insert(0, tree_lista[4])
+        entry_prioridade.insert(0, tree_lista[5])
+        entry_especialista.insert(0, tree_lista[6])
+
+        def atualizados():
+            nome = entry_nome.get()
+            email = entry_email.get()
+            telefone = entry_telefone.get()
+            calendario = entry_calendario.get()
+            prioridade = entry_prioridade.get()
+            especialista = entry_especialista.get()
+
+            lista = [nome, email, telefone, calendario, prioridade, especialista]
+
+            if nome == '':
+                messagebox.showerror('Erro', 'O nome não pode ser vazio')
+            else:
+                inserir_info(lista)
+                messagebox.showinfo('Sucesso', 'Os dados foram atualizados com sucesso')
+
+                entry_nome.delete(0, 'end')
+                entry_email.delete(0, 'end')
+                entry_telefone.delete(0, 'end')
+                entry_calendario.delete(0, 'end')
+                entry_prioridade.delete(0, 'end')
+                entry_especialista.delete(0, 'end')
+
+            for widget in frame_direita.winfo_children():
+                widget.destroy()
+
+            mostrar()
+
+
 # Configuração tela preenchimento
 # Nome
 label_nome = Label(frame_baixo, text='Nome', anchor=NW, font=('Ivy 10 bold'), bg=cor1, fg=cor4, relief='flat')
@@ -68,53 +150,53 @@ entry_prioridade = Entry(frame_baixo, width=21, justify='left', relief='solid')
 entry_prioridade.place(x=160, y=220)
 
 # Especialista
-label_nome = Label(frame_baixo, text='Especialista', anchor=NW, font=('Ivy 10 bold'), bg=cor1, fg=cor4, relief='flat')
-label_nome.place(x=15, y=260)
-entry_nome = Entry(frame_baixo, width=45, justify='left', relief='solid')
-entry_nome.place(x=15, y=290)
+label_especialista = Label(frame_baixo, text='Especialista', anchor=NW, font=('Ivy 10 bold'), bg=cor1, fg=cor4, relief='flat')
+label_especialista.place(x=15, y=260)
+entry_especialista = Entry(frame_baixo, width=45, justify='left', relief='solid')
+entry_especialista.place(x=15, y=290)
 
 # Botões
-botao_inserir = Button(frame_baixo, text='Inserir', width=10, font=('Ivy 8 bold'), bg=cor6, fg=cor1, relief='raised', overrelief='ridge')
+botao_inserir = Button(frame_baixo, command=inserir,text='Inserir', width=10, font=('Ivy 8 bold'), bg=cor6, fg=cor1, relief='raised', overrelief='ridge')
 botao_inserir.place(x=15, y=340)
 botao_atualizar = Button(frame_baixo, text='Atualizar', width=10, font=('Ivy 8 bold'), bg=cor2, fg=cor1, relief='raised', overrelief='ridge')
 botao_atualizar.place(x=112, y=340)
 botao_deletar = Button(frame_baixo, text='Deletar', width=10, font=('Ivy 8 bold'), bg=cor7, fg=cor1, relief='raised', overrelief='ridge')
 botao_deletar.place(x=210, y=340)
 
-# Tabelas
-lista = [[1, 'Fulano', 'fulano@gmail.com', 123456789, '27/06/2022', 'Normal', 'Clínico Geral'],
-         [2, 'Beltrano', 'beltrano@gmail.com', 123456789, '27/06/2022', 'Normal', 'Clínico Geral'],
-         [3, 'Siclano', 'siclano@gmail.com', 123456789, '27/06/2022', 'Normal', 'Clínico Geral']
-         ]
+def mostrar():
+    global tree
+    # Tabelas
+    lista = mostrar_info()
 
-nomes_listas = ['ID', 'Nome', 'E-mail', 'Telefone', 'Data', 'Prioridade', 'Especialista']
+    nomes_listas = ['ID', 'Nome', 'E-mail', 'Telefone', 'Data', 'Prioridade', 'Especialista']
 
-tree = ttk.Treeview(frame_direita, selectmode='extended', columns=nomes_listas, show='headings')
+    tree = ttk.Treeview(frame_direita, selectmode='extended', columns=nomes_listas, show='headings')
 
-vertical_scrollbar = ttk.Scrollbar(frame_direita, orient='vertical', command=tree.xview)
+    vertical_scrollbar = ttk.Scrollbar(frame_direita, orient='vertical', command=tree.xview)
 
-horizontal_scrollbar = ttk.Scrollbar(frame_direita, orient='horizontal', command=tree.xview)
+    horizontal_scrollbar = ttk.Scrollbar(frame_direita, orient='horizontal', command=tree.xview)
 
-tree.configure(yscrollcommand=vertical_scrollbar.set, xscrollcommand=horizontal_scrollbar.set)
+    tree.configure(yscrollcommand=vertical_scrollbar.set, xscrollcommand=horizontal_scrollbar.set)
 
-tree.grid(column=0, row=0, sticky='NSEW')
-vertical_scrollbar.grid(column=1, row=0, sticky='NS')
-horizontal_scrollbar.grid(column=0, row=1, sticky='EW')
+    tree.grid(column=0, row=0, sticky='NSEW')
+    vertical_scrollbar.grid(column=1, row=0, sticky='NS')
+    horizontal_scrollbar.grid(column=0, row=1, sticky='EW')
 
-frame_direita.grid_rowconfigure(0, weight=12)
+    frame_direita.grid_rowconfigure(0, weight=12)
 
-identacao_lista = ['nw', 'nw', 'nw', 'nw', 'nw', 'center', 'center']
-tamanho_tabela = [30, 170, 140, 100, 120, 50, 100]
-n=0
+    identacao_lista = ['nw', 'nw', 'nw', 'nw', 'nw', 'center', 'center']
+    tamanho_tabela = [30, 170, 140, 100, 120, 50, 100]
+    n=0
 
-for coluna in nomes_listas:
-    tree.heading(coluna, text=coluna.title(), anchor=CENTER)
-    tree.column(coluna, width=tamanho_tabela[n], anchor=identacao_lista[n])
+    for coluna in nomes_listas:
+        tree.heading(coluna, text=coluna.title(), anchor=CENTER)
+        tree.column(coluna, width=tamanho_tabela[n], anchor=identacao_lista[n])
 
-    n += 1
+        n += 1
 
-for item in lista:
-    tree.insert('', 'end', values=item)
+    for item in lista:
+        tree.insert('', 'end', values=item)
 
+mostrar()
 
 janela.mainloop()
--- view.py
+++ view.py
@@ -5,22 +5,26 @@ import sqlite3 as lite
 # Criando a conexão
 conexao = lite.connect('dados.db')
 
-lista = ['Fulano', 'fulano@gmail.com', 123456789, '27/06/2022', 'Normal', 'Clínico Geral']
-
 # Inserir
-with conexao:
-    cursor = conexao.cursor()
-    query = 'INSERT INTO formulario (nome, email, telefone, dia_consulta, prioridade, assunto) VALUES(?, ?, ?, ?, ?, ?)'
-    cursor.execute(query, lista)
+def inserir_info(i):
+    with conexao:
+        cursor = conexao.cursor()
+        query = 'INSERT INTO formulario (nome, email, telefone, dia_consulta, prioridade, assunto) VALUES(?, ?, ?, ?, ?, ?)'
+        cursor.execute(query, i)
 
 # Acessar Informações
-with conexao:
-    cursor = conexao.cursor()
-    query = 'SELECT * FROM formulario'
-    cursor.execute(query)
-    info = cursor.fetchall()
-    print(info)
+def mostrar_info():
+    lista = []
+    with conexao:
+        cursor = conexao.cursor()
+        query = 'SELECT * FROM formulario'
+        cursor.execute(query)
+        informacao = cursor.fetchall()
 
+        for infos in informacao:
+            lista.append(infos)
+    return lista
+'''
 # Atualizar
 with conexao:
     cursor = conexao.cursor()
@@ -33,3 +37,4 @@ with conexao:
     query = 'DELETE FROM formulario WHERE id=?'
     cursor.execute(query, lista)
 
+'''
